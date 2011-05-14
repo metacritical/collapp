5c48d3b3664e8e918b55fd74ab9b44ee442c6920
Pankaj Doharey <pankajdoharey@live.com>
2011-05-14 23:47:46 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Collapp::Application.config.secret_token = 'd8eb3c307f8d6e234e84a5d59d410c677f53302607feb298d054aee9c1a69388e6e7d5f542955eba2df899ac554f81fc24382b42aa73c0c1ebc626c980d6e2fb'
